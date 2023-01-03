62401e86ffee4060956b630a80d396dc787435ba
PS <psolorzn@yahoo.com>
2023-01-03 18:07:27 -0500

    Add files via upload

---
+++ views/inicio.php
@@ -0,0 +1,12 @@
+<h1>Portal de inicio</h1>
+<hr>
+<ul>
+    <li>Lunes</li>
+    <li>Martes</li>
+    <li>Miércoles</li>
+    <li>Jueves</li>
+    <li>Viernes</li>
+</ul>
+<?php
+echo '<h4>Pie de página</h4>';
+?>
